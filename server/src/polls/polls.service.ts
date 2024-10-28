99aaa1bc49466a03377970eab6968e1633e520fb
Ashim <ashimshrestha2384@gmail.com>
2024-10-28 20:57:42 +0545

    PollsService and ids

--- server/src/polls/polls.service.ts
+++ server/src/polls/polls.service.ts
@@ -1,11 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePollFields, JoinPollFields, RejoinPollFields } from './types';
+import { createPollId, createUserId } from 'src/ids';
 
 @Injectable()
 export class PollsService {
-  async createPoll(fields: CreatePollFields) {}
+  async createPoll(fields: CreatePollFields) {
+    const pollID = createPollId();
+    const userID = createUserId();
 
-  async joinPoll(fields: JoinPollFields) {}
+    return { ...fields, pollID, userID };
+  }
 
-  async rejoinPoll(fields: RejoinPollFields) {}
+  async joinPoll(fields: JoinPollFields) {
+    const userID = createUserId();
+
+    return { ...fields, userID };
+  }
+
+  async rejoinPoll(fields: RejoinPollFields) {
+    return fields;
+  }
 }
