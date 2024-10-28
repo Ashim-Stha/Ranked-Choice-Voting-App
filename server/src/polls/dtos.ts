83788f1be87e16a68ff0dc5c6840467ac83f4bd0
Ashim <ashimshrestha2384@gmail.com>
2024-10-28 19:51:06 +0545

    PollsService and types

--- server/src/polls/dtos.ts
+++ server/src/polls/dtos.ts
@@ -19,4 +19,8 @@ export class JoinPollDto {
   @IsString()
   @Length(6, 6)
   pollID: string;
+
+  @IsString()
+  @Length(1, 25)
+  name: string;
 }
--- server/src/polls/polls.module.ts
+++ server/src/polls/polls.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PollsController } from './polls.controller';
+import { PollsService } from './polls.service';
 
 @Module({
   imports: [ConfigModule], //configModule allows to access env variables inside polls folder
   controllers: [PollsController],
-  providers: [],
+  providers: [PollsService],
 })
 export class PollsModule {}
