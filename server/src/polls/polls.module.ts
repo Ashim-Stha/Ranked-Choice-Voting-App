67db9ce39ce70dc3dad52089142429a7c19d1fe1
Ashim <ashimshrestha2384@gmail.com>
2024-10-29 20:57:43 +0545

    pollsRepository

--- server/src/polls/polls.module.ts
+++ server/src/polls/polls.module.ts
@@ -3,10 +3,11 @@ import { ConfigModule } from '@nestjs/config';
 import { PollsController } from './polls.controller';
 import { PollsService } from './polls.service';
 import { redisModule } from 'src/modules.config';
+import { PollsRepository } from './polls.repository';
 
 @Module({
   imports: [ConfigModule, redisModule], //configModule allows to access env variables inside polls folder
   controllers: [PollsController],
-  providers: [PollsService],
+  providers: [PollsService, PollsRepository],
 })
 export class PollsModule {}
--- server/src/polls/polls.service.ts
+++ server/src/polls/polls.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePollFields, JoinPollFields, RejoinPollFields } from './types';
 import { createPollId, createUserId } from 'src/ids';
+import { PollsRepository } from './polls.repository';
 
 @Injectable()
 export class PollsService {
+  constructor(private readonly pollsRepository: PollsRepository) {}
   async createPoll(fields: CreatePollFields) {
     const pollID = createPollId();
     const userID = createUserId();
