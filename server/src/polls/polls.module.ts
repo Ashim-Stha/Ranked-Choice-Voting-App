6f3a4edad99ba5854388decff88df335f243ae42
Ashim <ashimshrestha2384@gmail.com>
2024-10-29 14:33:01 +0545

    modules config

--- server/src/polls/polls.module.ts
+++ server/src/polls/polls.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PollsController } from './polls.controller';
 import { PollsService } from './polls.service';
+import { redisModule } from 'src/modules.config';
 
 @Module({
-  imports: [ConfigModule], //configModule allows to access env variables inside polls folder
+  imports: [ConfigModule, redisModule], //configModule allows to access env variables inside polls folder
   controllers: [PollsController],
   providers: [PollsService],
 })
