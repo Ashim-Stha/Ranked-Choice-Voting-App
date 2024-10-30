fa5f4702504f45deab5799c5cc20cd66dd3f00db
Ashim <ashimshrestha2384@gmail.com>
2024-10-30 15:43:04 +0545

    controllerAuthGuard service

---
+++ server/src/polls/controller-auth.guard.ts
@@ -0,0 +1,20 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  Logger,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class ControllerAuthGuard implements CanActivate {
+  private readonly logger = new Logger(ControllerAuthGuard.name);
+  constructor(private readonly jwtService: JwtService) {}
+
+  canActivate(context: ExecutionContext): boolean | Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    this.logger.debug(`Checking for auth token on request body `, request.body);
+    return false;
+  }
+}
