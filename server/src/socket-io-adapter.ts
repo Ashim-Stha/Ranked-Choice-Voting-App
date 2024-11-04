018006f5b3555a07b1af39464152dd4db52475be
Ashim <ashimshrestha2384@gmail.com>
2024-11-04 16:24:07 +0545

    socket.io middleware

--- server/src/socket-io-adapter.ts
+++ server/src/socket-io-adapter.ts
@@ -1,13 +1,15 @@
 import { INestApplicationContext, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 import { IoAdapter } from '@nestjs/platform-socket.io';
-import { ServerOptions } from 'socket.io';
+import { Server, ServerOptions } from 'socket.io';
+import { SocketWihAuth } from './polls/types';
 
 export class SocketIOAdapter extends IoAdapter {
   private readonly logger = new Logger(SocketIOAdapter.name);
   constructor(
     private app: INestApplicationContext,
-    private readonly configService: ConfigService,
+    private configService: ConfigService,
   ) {
     super(app);
   }
@@ -17,20 +19,43 @@ export class SocketIOAdapter extends IoAdapter {
 
     const cors = {
       origin: [
-        'http://localhost:8080',
+        `http://localhost:${clientPort}`,
         new RegExp(`/^http:\/\/192\.168\.1\.([1-9]|[1-9]\d):${clientPort}$/`),
       ],
     };
 
-    this.logger.log(`Configuring SocketIO server with custom CORS options`, {
+    this.logger.log('Configuring SocketIO server with custom CORS options', {
       cors,
     });
 
-    const optionsWithCors: ServerOptions = {
+    const optionsWithCORS: ServerOptions = {
       ...options,
       cors,
     };
 
-    return super.createIOServer(port, optionsWithCors);
+    const jwtService = this.app.get(JwtService);
+    const server: Server = super.createIOServer(port, optionsWithCORS);
+
+    server.of('polls').use(createTokenMiddleware(jwtService, this.logger));
+
+    return server;
   }
 }
+
+const createTokenMiddleware =
+  (jwtService: JwtService, logger: Logger) => (socket: SocketWihAuth, next) => {
+    const token =
+      socket.handshake.auth.token || socket.handshake.headers['token'];
+
+    logger.debug(`Validating auth token before connection: ${token}`);
+
+    try {
+      const payload = jwtService.verify(token);
+      socket.userID = payload.sub;
+      socket.pollID = payload.pollID;
+      socket.name = payload.name;
+      next();
+    } catch {
+      next(new Error('FORBIDDEN'));
+    }
+  };
