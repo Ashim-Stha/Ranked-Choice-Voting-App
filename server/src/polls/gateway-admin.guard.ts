9cbeca8ee92fd5401832d92a03d6c90f2fac0238
Ashim <ashimshrestha2384@gmail.com>
2024-11-10 22:24:22 +0545

    Admin Authorization Guard

---
+++ server/src/polls/gateway-admin.guard.ts
@@ -0,0 +1,43 @@
+import { CanActivate, ExecutionContext, Logger } from '@nestjs/common';
+import { PollsService } from './polls.service';
+import { JwtService } from '@nestjs/jwt';
+import { AuthPayload, SocketWihAuth } from './types';
+import { WsUnauthorizedException } from 'src/exceptions/ws-exception';
+
+export class GatewayAdminGuard implements CanActivate {
+  private readonly logger = new Logger(GatewayAdminGuard.name);
+  constructor(
+    private readonly pollsService: PollsService,
+    private readonly jwtService: JwtService,
+  ) {}
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const socket: SocketWihAuth = context.switchToWs().getClient();
+
+    const token =
+      socket.handshake.auth.token || socket.handshake.headers['token'];
+
+    if (!token) {
+      this.logger.error('No authorization token provided');
+      throw new WsUnauthorizedException('No token provided');
+    }
+
+    try {
+      const payload = this.jwtService.verify<AuthPayload & { sub: string }>(
+        token,
+      );
+
+      this.logger.debug(`Validating admin using token payload: ${payload}`);
+
+      const { userID, pollID } = payload;
+      const poll = await this.pollsService.getPoll(pollID);
+
+      if (userID !== poll.adminID) {
+        throw new WsUnauthorizedException('Admin privileges required');
+      }
+
+      return true;
+    } catch {
+      throw new WsUnauthorizedException('Admin privileges required');
+    }
+  }
+}
